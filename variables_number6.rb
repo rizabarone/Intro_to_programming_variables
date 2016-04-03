93eef44f275afd5b93dfab1c8e7e18a656630a00
Riza Barone <ysa_e@hotmail.com>
2016-04-02 20:54:26 -0400

    add answers to variables exercises

---
+++ variables_number6.rb
@@ -0,0 +1,7 @@
+# 6.What does the following error message tell you?
+
+#NameError: undefined local variable or method `shoes' for main:Object
+# from (irb):3
+# from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+
+# Answer: the message tells me that the shoes is not available outside the scope and it wasn't defined.
